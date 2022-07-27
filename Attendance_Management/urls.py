0da8e3848d43a61f7ec8611a70cacd912ddcaa7c
parthiban m <97148433+parthiban777@users.noreply.github.com>
2022-07-27 16:41:56 +0530

    qq

---
+++ Attendance_Management/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url,include
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',TemplateView.as_view(template_name='index.html')),
+    url('attendance/',include('AttendanceApp.urls'))
+]
